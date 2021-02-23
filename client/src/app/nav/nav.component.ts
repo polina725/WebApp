e4b9133d3ef9a173279e24616a922dfc24c05c70
Palina Hubko <polinagubcko@gmail.com>
2021-02-23 14:42:39 +0300

    error handling

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ToastrModule, ToastrService } from 'ngx-toastr';
-import { Observable } from 'rxjs';
-import { User } from '../_models/user';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -21,9 +19,6 @@ export class NavComponent implements OnInit {
   login(): void{
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('members');
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
     })
   }
 

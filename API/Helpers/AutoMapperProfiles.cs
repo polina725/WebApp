32aab876420cc056e81389bd584d5af6150173a5
Palina Hubko <polinagubcko@gmail.com>
2021-02-26 13:12:34 +0300

    profile editing added

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -16,6 +16,7 @@ namespace API.Helpers
             .ForMember(dest => dest.Age, opt => opt.MapFrom(src => 
                 src.DateOfBirth.CalcAge()));
             CreateMap<Photo, PhotoDto>();
+            CreateMap<MemberUpdateDto,AppUser>();
         }
     }
 }
